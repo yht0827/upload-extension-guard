30293ec8419f5cd91480968f615c800a839bb3f1
yht0827 <yangheetae0827@naver.com>
2026-02-06 19:02:35 +0900

    initial commit

---
+++ src/main/java/com/example/uploadextensionguard/UploadExtensionGuardApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadextensionguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadExtensionGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadExtensionGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uploadextensionguard/UploadExtensionGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadextensionguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadExtensionGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
